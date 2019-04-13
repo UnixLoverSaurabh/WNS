0f229e6f1a26a2efaaaf1c804ef26665eef45f29
UnixLoverSaurabh <saurabh4104@gmail.com>
2019-04-13 19:11:11 +0530

    communication between client server with objectSream

--- Client/src/sample/Main.java
+++ Client/src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Main {
@@ -9,6 +11,23 @@ public class Main {
 
         Socket socket = new Socket("localhost", 5700);
 
-        System.out.println("Client connected");
+        try {
+            System.out.println("Client is first sending message");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject("A new message from Client");
+            objectOutputStream.flush();
+
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            String receivedMessage = (String)objectInputStream.readObject();
+            System.out.println("\nReceived Server Message : " + receivedMessage);
+
+        } catch (IOException e) {
+            System.out.println("Error in socket.getInputStream() ");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error in objectInputStream.readObject() ");
+            e.printStackTrace();
+        }
     }
 }
--- Server/src/sample/ServerThread.java
+++ Server/src/sample/ServerThread.java
@@ -1,5 +1,8 @@
 package sample;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ServerThread extends Thread{
@@ -13,6 +16,23 @@ public class ServerThread extends Thread{
     @Override
     public void run() {
 
-        System.out.println("Server has given a thread to client");
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(this.socket.getInputStream());
+            String receivedMessage = (String)objectInputStream.readObject();
+            System.out.println("Received Client Message : " + receivedMessage);
+
+
+            System.out.println("\nServer is giving response message");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject("Response from server");
+            objectOutputStream.flush();
+
+        } catch (IOException e) {
+            System.out.println("Error in socket.getInputStream() ");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error in objectInputStream.readObject() ");
+            e.printStackTrace();
+        }
     }
 }
